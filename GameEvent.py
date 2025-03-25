b6a981cdd7f5b5241ea6c67ea879e57d7ba9bb12
Charlie Brunold <cbrunold@usc.edu>
2025-03-24 22:34:55 -0700

    make pseudocode for pitch handling

--- GameEvent.py
+++ GameEvent.py
@@ -18,5 +18,28 @@ class GameEvent:
         """
         Records a pitch event in the game. At this point in development, every event will happen after a pitch.
         """
+        # pitch is a strike
+            # check if pitch is a called strike
+            # otherwise, pitch is a swinging strike
+            # regardless, increment the strike count (helper function to check if batter strikes out)
+            # if runners on base, cehck if they advance (helper function?)
+
+        # pitch is a ball
+            # ball came from a wild pitch
+                # increment count and check if there is a walk (helper function)
+                # if runners on base, check if they advance (helper function?)
+            # ball came from a passed ball
+                # increment count and check if there is a walk (helper function)
+                # if runners on base, check if they advance (helper function?)
+
+        # batter makes contact with the ball
+            # check if the ball is a foul ball
+            # otherwise, the ball is a hit
+                # check if the ball is hard hit or weakly hit
+                    # check if the ball is a ground ball  
+                    # check if the ball is a line drive
+                    # check if the ball is a fly ball
+                    # check if the ball is a pop up
+
 
 
